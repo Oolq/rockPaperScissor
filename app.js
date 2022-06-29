94f374e33795bf3fea0d2c7de79c5a71d718c8d3
jamez <christianheramis02@gmail.com>
2022-06-29 23:04:44 +0800

    playing with javascript

---
+++ app.js
@@ -0,0 +1,50 @@
+console.log('hello world');
+
+//FIZZBUZZ
+/*
+for (let i = 1; i <=   100; i++) {
+  if (i % 5 == 0 && i % 3 == 0 ) {
+    console.log('FizzBuzz');
+  } else if (i % 5 == 0) {
+    console.log('Buzz');
+  } else if ( i % 3 == 0 ) {
+    console.log('Fizz');
+  } else{
+    console.log(i);
+  }
+}
+
+for (let n = 1; n <= 100; n++) {
+  let output = "";
+  if (n % 3 == 0) output += "Fizz";
+  if (n % 5 == 0) output += "Buzz";
+  console.log(output || n);
+}
+*/
+
+//LOOPING A TRIANGLE
+/*for (let line = "#"; line.length < 8; line += "#")
+  console.log(line);
+  */
+
+  //CHESSBOARD
+
+ /* let size = 10;
+
+  let board = "";
+  
+  for (let y = 0; y < size; y++) {
+    for (let x = 0; x < size; x++) {
+      if ((x + y) % 2 == 0) {
+        board += " ";
+      } else {
+        board += "#";
+      }
+    }
+    board += "\n";
+  }
+  
+  console.log(board);
+  */
+
+  
