f5f50d9333c14af1027034c3649fd2c8ba5fb335
jamez <christianheramis02@gmail.com>
2022-07-06 23:42:12 +0800

    adding scoring

--- app.js
+++ app.js
@@ -46,5 +46,63 @@ for (let n = 1; n <= 100; n++) {
   
   console.log(board);
   */
+ let player
+ let computer
+ let playerscore = 0;
+ let computerscore = 0;
+ 
+  
+  function gameRound(playerChoice, computerChoice){
+  let choices = ['rock', 'paper', 'scissor'];
+ computerChoice = choices[Math.floor(Math.random()* choices.length)];
+  console.log('Computer:' + computerChoice);
+  console.log('Player:' + playerChoice);
+
+  if(playerChoice == 'scissor' && computerChoice == 'rock'|| playerChoice == 'rock' && computerChoice == 'paper' || playerChoice == 'paper' && computerChoice == 'scissor'){
+    console.log('YOU LOST THIS ROUND!!!');
+    player = 'lost';
+    computer ='winner';
+    scoring();
+  } else if(computerChoice == playerChoice){
+    console.log('IT\'S A DRAW!!!');
+    player && computer === 'draw';
+    scoring();
+
+  }else{
+    console.log('YOU WIN THIS ROUND!!!');
+    player = 'winner';
+    computer = 'lost';
+    scoring();
+  }
 
   
+  }
+
+  function scoring(){
+    
+    if(player == 'winner' && computer == 'lost'){
+      playerscore++;
+    } else if( player == 'lost' && computer == 'winner'){
+      computerscore++ ;
+    }else if(player && computer === 'draw'){
+      playerscore = playerscore;
+      computerscore = computerscore;
+    }else{}
+
+    console.log('Pscore:' + playerscore);
+    console.log('Cscore:' + computerscore);
+  }
+
+ 
+ gameRound('rock');
+ gameRound('paper');
+ gameRound('scissor');
+
+
+
+
+  
+ 
+
+
+
